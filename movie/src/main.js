7772f66f399428a9ff1fbbf72469035b6790407d
holdena12 <102497243+holdena12@users.noreply.github.com>
2024-02-27 10:10:24 -0700

    g

--- movie/src/main.js
+++ movie/src/main.js
@@ -3,7 +3,15 @@ let kidTP = 0
 let totalTP = 0
 let mutantTP = 0
 let trollTP = 0
+let trollTPK = 0
+let trollTPA = 0
 let spiderTP = 0
+let spiderTPK = 0
+let spiderTPA = 0
+let mutantTPK = 0
+let mutantTPA = 0
+let totalSpent = 0
+
 
 function hide(elementId){
   document.getElementById(elementId).classList.add("hide")
@@ -11,13 +19,52 @@ function hide(elementId){
 function unHide(elementId){
   document.getElementById(elementId).classList.remove("hide")
 }
-function getStats(totalTP,kidTP){
+
+function getStats(){
+  let trollsSpent = trollTPA*(10.49)
+  trollsSpent = trollsSpent + (trollTPK*7.49)
+  let spiderSpent = spiderTPA*(14.49)
+  spiderSpent = spiderSpent + (spiderTPK*11.24)
+  let mutantSpent = mutantTPA*(15.49)
+  mutantSpent = mutantSpent + (mutantTPK*12.49)
   let stats = document.getElementById("stats")
+  totalSpent = spiderSpent +trollsSpent + mutantSpent
+  totalSpent = totalSpent* 1.0875
   stats.innerHTML = `
   <div id = "stats">
   <p> Total tickets purchased: ${totalTP}</p>
   <p> Total child tickets purchased: ${kidTP}</p>
-  <button id="goHome">Home</button>`
+  <p> Total adult tickets purchased: ${adultTP}</p>
+  <p> Total spent in total: ${totalSpent}</p>
+
+
+  
+ `
+  if(mutantTP > 0 ){
+  stats.innerHTML +=
+  ` <p> Total tickets bought for Teenage Mutant Ninja Turtles: ${mutantTP}</p>
+  <p> Total adult tickets purchased for Teenage Mutant Ninja Turtles: ${mutantTPA}</p>
+  <p> Total child tickets purchased for Teenage Mutant Ninja Turtles: ${mutantTPK}</p>
+  <p> Total spent on Teenage Mutant Ninja Turtles: $ ${mutantSpent}`}
+  if(spiderTP > 0 ){
+    stats.innerHTML +=
+    `
+ 
+    <p> Total tickets bought for Spider Man: ${spiderTP}</p>
+    <p> Total adult tickets purchased for Spider Man: ${spiderTPA}</p>
+    <p> Total child tickets purchased for Spider Man: ${spiderTPK}</p>
+    <p> Total spent on Spider Man: $ ${spiderSpent}</p>`
+    }
+    if(trollTP > 0 ){
+      stats.innerHTML +=
+      `
+   
+      <p> Total tickets bought for Trolls: ${trollTP}</p>
+      <p> Total adult tickets purchased for Trolls: ${trollTPA}</p>
+      <p> Total child tickets purchased for Trolls: ${trollTPK}</p>
+      <p> Total spent on trolls: $ ${trollsSpent}</p>`
+      }
+  stats.innerHTML += ` <button id="goHome">Home</button>`
   document.getElementById("goHome").addEventListener("click",function(ev){
     hide("stats")
     unHide("homePage")
@@ -47,13 +94,8 @@ function checkout(pricea,pricek,movie){
 
   console.log(kidTP,adultTP,totalTP)
 }
-function countTicketsPurchased(element,typeP, movieP){
-  document.getElementById(element).addEventListener("click",function(ev){
-    typeP +=1
-    movieP +=1
-    totalTP +=1
-  })
-}
+
+
 
 
  
@@ -62,8 +104,20 @@ document.getElementById("tmnt").addEventListener("click",function(ev){
   hide("homePage")
   checkout("$15.49","$12.49",mutantTP)
   unHide("checkoutPage")
-  countTicketsPurchased("adultT","adultTP",mutantTP)
-  countTicketsPurchased("kidT","kidTP",mutantTP)
+  document.getElementById("kidT").addEventListener("click",function(ev){
+    kidTP+=1
+    totalTP+=1
+    mutantTP+=1
+    mutantTPK+=1
+
+  })
+ 
+  document.getElementById("adultT").addEventListener("click",function(ev){
+    adultTP+=1
+    totalTP+=1
+    mutantTP+=1
+    mutantTPA +=1
+  })
 
 
 
@@ -73,8 +127,20 @@ document.getElementById("smasv").addEventListener("click",function(ev){
   hide("homePage")
   checkout("$14.49","$11.24",spiderTP)
   unHide("checkoutPage")
-  countTicketsPurchased("adultT","adultTP",spiderTP)
-  countTicketsPurchased("kidT","kidTP",spiderTP)
+  document.getElementById("kidT").addEventListener("click",function(ev){
+    kidTP+=1
+    totalTP+=1
+    spiderTP+=1
+    spiderTPK+=1
+  })
+ 
+  document.getElementById("adultT").addEventListener("click",function(ev){
+    adultTP+=1
+    totalTP+=1
+    spiderTP+=1
+    spiderTPA+=1
+  })
+  
   
 
 
@@ -85,8 +151,20 @@ document.getElementById("tbt").addEventListener("click",function(ev){
   hide("homePage")
   checkout("$10.49","$7.49",trollTP)
   unHide("checkoutPage")
-  countTicketsPurchased("adultT","adultTP",trollTP)
-  countTicketsPurchased("kidT","kidTP",trollTP)
+  document.getElementById("kidT").addEventListener("click",function(ev){
+    kidTP+=1
+    totalTP+=1
+    trollTP+=1
+    trollTPK +=1
+  })
+ 
+  document.getElementById("adultT").addEventListener("click",function(ev){
+    adultTP+=1
+    totalTP+=1
+    trollTP+=1
+    trollTPA+=1
+  })
+  
   
 
 
@@ -95,7 +173,7 @@ document.getElementById("tbt").addEventListener("click",function(ev){
 })
 document.getElementById("goToStats").addEventListener("click",function(ev){
   hide("homePage")
-  getStats(totalTP)
+  getStats(totalTP,kidTP)
 
 
   
