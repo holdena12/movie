923158cc7ecccc78d71fbb38e8dc49719a4cd881
holdena12 <102497243+holdena12@users.noreply.github.com>
2024-02-27 13:55:22 -0700

    I tryed

--- movie/src/main.js
+++ movie/src/main.js
@@ -1,35 +1,98 @@
-let adultTP = 0
-let kidTP = 0
-let totalTP = 0
-let mutantTP = 0
-let trollTP = 0
-let trollTPK = 0
-let trollTPA = 0
-let spiderTP = 0
-let spiderTPK = 0
-let spiderTPA = 0
-let mutantTPK = 0
-let mutantTPA = 0
-let totalSpent = 0
 
 
-function hide(elementId){
+const movies = []
+
+const json = await(await fetch('./data.json')).json()
+console.log(json)
+for (let movie of json.movies) {
+  const movieObject = new Movie(
+    movie.adultPrice,
+    movie.childPrice,
+    movie.title,
+    movie.id
+  )
+  movies.push(movieObject)
+
+
+}
+
+class Movie {
+  adultTicketPrice;
+  childTicketPrice;
+  movieTitle;
+  id;
+
+  adultTicketsBought = 0;
+  childTicketsBought = 0;
+
+  amountSpentAdult() {
+    return this.adultTicketPrice * this.adultTicketsBought
+  }
+  amountSpentChild() {
+    returnthis.childTicketPrice * this.childTicketsBought
+  }
+  totalTicketsBought() {
+    this.adultTicketsBought + this.childTicketsBought
+  }
+
+  constructor(
+    adultTicketPrice,
+    childTicketPrice,
+    movieTitle,
+    id,
+  ) {
+    this.adultTicketPrice = adultTicketPrice
+    this.childTicketPrice = childTicketPrice
+    this.movieTitle = movieTitle
+    this.id = id
+  }
+}
+
+function makeButton(title, id) {
+  const homePage = document.getElementById("homePage")
+  homePage.innerHTML = `
+  <button id = "${id}">${title}</button>`
+}
+for (let movie of movies) {
+  console.log(movie)
+  makeButton(movie.movieTitle, movie.id)
+}
+function getMovie(id) {
+  for (let movie of movies) {
+    if (movie.id == id) return movie
+  }
+}
+
+function hide(elementId) {
   document.getElementById(elementId).classList.add("hide")
 }
-function unHide(elementId){
+function unHide(elementId) {
   document.getElementById(elementId).classList.remove("hide")
 }
 
-function getStats(){
-  let trollsSpent = trollTPA*(10.49)
-  trollsSpent = trollsSpent + (trollTPK*7.49)
-  let spiderSpent = spiderTPA*(14.49)
-  spiderSpent = spiderSpent + (spiderTPK*11.24)
-  let mutantSpent = mutantTPA*(15.49)
-  mutantSpent = mutantSpent + (mutantTPK*12.49)
+function getStats() {
+
+  for (let movie of movies) {
+    const adultSpent = movie.amountSpentAdult()
+    const childSpent = movie.amountSpentChild()
+
+    let stats = document.getElementById("stats")
+
+    stats.innerHTML += `
+    <p> Adult tickets bought for ${movie.title} is :${movie.adultTicketsBought}</p>
+    <p> Child tickets bought for ${movie.title} is :${movie.childTicketsBought}</p>
+    <p> Total tickets bought for ${movie.title} is :${movie.totalTicketsBought}</p>`
+  }
+
+  let trollsSpent = trollTPA * (10.49)
+  trollsSpent = trollsSpent + (trollTPK * 7.49)
+  let spiderSpent = spiderTPA * (14.49)
+  spiderSpent = spiderSpent + (spiderTPK * 11.24)
+  let mutantSpent = mutantTPA * (15.49)
+  mutantSpent = mutantSpent + (mutantTPK * 12.49)
+  totalSpent = spiderSpent + trollsSpent + mutantSpent
   let stats = document.getElementById("stats")
-  totalSpent = spiderSpent +trollsSpent + mutantSpent
-  totalSpent = totalSpent* 1.0875
+  totalSpent = totalSpent * 1.0875
   stats.innerHTML = `
   <div id = "stats">
   <p> Total tickets purchased: ${totalTP}</p>
@@ -40,44 +103,45 @@ function getStats(){
 
   
  `
-  if(mutantTP > 0 ){
-  stats.innerHTML +=
-  ` <p> Total tickets bought for Teenage Mutant Ninja Turtles: ${mutantTP}</p>
+  if (mutantTP > 0) {
+    stats.innerHTML +=
+      ` <p> Total tickets bought for Teenage Mutant Ninja Turtles: ${mutantTP}</p>
   <p> Total adult tickets purchased for Teenage Mutant Ninja Turtles: ${mutantTPA}</p>
   <p> Total child tickets purchased for Teenage Mutant Ninja Turtles: ${mutantTPK}</p>
-  <p> Total spent on Teenage Mutant Ninja Turtles: $ ${mutantSpent}`}
-  if(spiderTP > 0 ){
+  <p> Total spent on Teenage Mutant Ninja Turtles: $ ${mutantSpent}`
+  }
+  if (spiderTP > 0) {
     stats.innerHTML +=
-    `
+      `
  
     <p> Total tickets bought for Spider Man: ${spiderTP}</p>
     <p> Total adult tickets purchased for Spider Man: ${spiderTPA}</p>
     <p> Total child tickets purchased for Spider Man: ${spiderTPK}</p>
     <p> Total spent on Spider Man: $ ${spiderSpent}</p>`
-    }
-    if(trollTP > 0 ){
-      stats.innerHTML +=
+  }
+  if (trollTP > 0) {
+    stats.innerHTML +=
       `
    
       <p> Total tickets bought for Trolls: ${trollTP}</p>
       <p> Total adult tickets purchased for Trolls: ${trollTPA}</p>
       <p> Total child tickets purchased for Trolls: ${trollTPK}</p>
       <p> Total spent on trolls: $ ${trollsSpent}</p>`
-      }
+  }
   stats.innerHTML += ` <button id="goHome">Home</button>`
-  document.getElementById("goHome").addEventListener("click",function(ev){
+  document.getElementById("goHome").addEventListener("click", function (ev) {
     hide("stats")
     unHide("homePage")
   })
   unHide("stats")
 }
-function checkout(pricea,pricek,movie){
+function checkout(pricea, pricek, movie) {
 
 
-  
-  
+
+
   let div = document.getElementById("checkoutPage")
-  div.innerHTML =` 
+  div.innerHTML = ` 
   <div id = "checkoutPage" >
   <div id="${movie}">
   <button id="adultT">Buy for adult: ${pricea}</button>
@@ -86,97 +150,97 @@ function checkout(pricea,pricek,movie){
   </div>
   </div>
   `
-  
-  document.getElementById("homeButton").addEventListener("click",function(ev){
+
+  document.getElementById("homeButton").addEventListener("click", function (ev) {
     unHide("homePage")
     hide("checkoutPage")
   })
 
-  console.log(kidTP,adultTP,totalTP)
+  console.log(kidTP, adultTP, totalTP)
 }
 
 
 
 
- 
 
-document.getElementById("tmnt").addEventListener("click",function(ev){
+
+document.getElementById("tmnt").addEventListener("click", function (ev) {
   hide("homePage")
-  checkout("$15.49","$12.49",mutantTP)
+  checkout("$15.49", "$12.49", mutantTP)
   unHide("checkoutPage")
-  document.getElementById("kidT").addEventListener("click",function(ev){
-    kidTP+=1
-    totalTP+=1
-    mutantTP+=1
-    mutantTPK+=1
+  document.getElementById("kidT").addEventListener("click", function (ev) {
+    kidTP += 1
+    totalTP += 1
+    mutantTP += 1
+    mutantTPK += 1
 
   })
- 
-  document.getElementById("adultT").addEventListener("click",function(ev){
-    adultTP+=1
-    totalTP+=1
-    mutantTP+=1
-    mutantTPA +=1
+
+  document.getElementById("adultT").addEventListener("click", function (ev) {
+    adultTP += 1
+    totalTP += 1
+    mutantTP += 1
+    mutantTPA += 1
   })
 
 
 
 
 })
-document.getElementById("smasv").addEventListener("click",function(ev){
+document.getElementById("smasv").addEventListener("click", function (ev) {
   hide("homePage")
-  checkout("$14.49","$11.24",spiderTP)
+  checkout("$14.49", "$11.24", spiderTP)
   unHide("checkoutPage")
-  document.getElementById("kidT").addEventListener("click",function(ev){
-    kidTP+=1
-    totalTP+=1
-    spiderTP+=1
-    spiderTPK+=1
+  document.getElementById("kidT").addEventListener("click", function (ev) {
+    kidTP += 1
+    totalTP += 1
+    spiderTP += 1
+    spiderTPK += 1
   })
- 
-  document.getElementById("adultT").addEventListener("click",function(ev){
-    adultTP+=1
-    totalTP+=1
-    spiderTP+=1
-    spiderTPA+=1
+
+  document.getElementById("adultT").addEventListener("click", function (ev) {
+    adultTP += 1
+    totalTP += 1
+    spiderTP += 1
+    spiderTPA += 1
   })
-  
-  
+
+
 
 
 
 
 })
-document.getElementById("tbt").addEventListener("click",function(ev){
+document.getElementById("tbt").addEventListener("click", function (ev) {
   hide("homePage")
-  checkout("$10.49","$7.49",trollTP)
+  checkout("$10.49", "$7.49", trollTP)
   unHide("checkoutPage")
-  document.getElementById("kidT").addEventListener("click",function(ev){
-    kidTP+=1
-    totalTP+=1
-    trollTP+=1
-    trollTPK +=1
+  document.getElementById("kidT").addEventListener("click", function (ev) {
+    kidTP += 1
+    totalTP += 1
+    trollTP += 1
+    trollTPK += 1
   })
- 
-  document.getElementById("adultT").addEventListener("click",function(ev){
-    adultTP+=1
-    totalTP+=1
-    trollTP+=1
-    trollTPA+=1
+
+  document.getElementById("adultT").addEventListener("click", function (ev) {
+    adultTP += 1
+    totalTP += 1
+    trollTP += 1
+    trollTPA += 1
   })
-  
-  
+
+
 
 
 
 
 })
-document.getElementById("goToStats").addEventListener("click",function(ev){
+document.getElementById("goToStats").addEventListener("click", function (ev) {
   hide("homePage")
-  getStats(totalTP,kidTP)
+  getStats(totalTP, kidTP)
 
 
-  
+
 })
 
 
