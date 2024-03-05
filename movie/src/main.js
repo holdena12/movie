5c96495a93e1473e9a5eb4e31b4587f1b6c534c9
holdena12 <102497243+holdena12@users.noreply.github.com>
2024-03-04 19:11:40 -0700

    Reverted back to previous working commit.

--- movie/src/main.js
+++ movie/src/main.js
@@ -57,9 +57,73 @@ for (let movie of json.movies) {
 let completeTicketsBought =0;
 
 for (let movie of movies) {
-  console.log(movie)
-  makeButton(movie.movieTitle, movie.id)
+  makeButton(movie);
+  
+  
 }
+
+for(let movie of movies){
+  document.getElementById(movie.id).addEventListener("click",function(ev){
+    checkOutPage(movie);
+  });
+}
+
+// TODO: call this when stats page is loading
+//for (let movie of movies){
+//  countTickets(movie)
+//}
+getCompleteTicketsPurchased()
+
+function checkOutPage(movie) {
+  document.getElementById("checkoutPage").innerHTML = `
+      <p>${movie.movieTitle}</p>
+      <button id ="${movie.id}k" class="btn">Buy for a kid: ${movie.childTicketPrice}</button>
+      <button id = "${movie.id}a" class="btn">Buy for an adult: ${movie.adultTicketPrice}</button>
+      <button id ="homeButton" class="btn">Home</button>
+  `;
+
+  hide("homePage");
+  unHide("checkoutPage");
+
+  document.getElementById("homeButton").addEventListener("click", function(ev) {
+      hide("checkoutPage");
+      unHide("homePage");
+  });
+ 
+
+  addTicketPurchaseListner(movie)
+
+}
+  // Add event listeners for buying tickets
+function addTicketPurchaseListner(movie){
+  console.log("counting movie with id: " + movie.id)
+  document.getElementById(`${movie.id}k`).addEventListener("click", function(ev) {
+      movie.purchaseChildTicket()
+      console.log("child ticket purchased for " + movie.childTicketPrice + " total tickets bought = " + movie.getTotalTicketsBought())
+
+  });
+
+  document.getElementById(`${movie.id}a`).addEventListener("click", function(ev) {
+    movie.purchaseAdultTicket()
+    console.log("adult ticket purchased for " + movie.adultTicketPrice + " total tickets bought = " + movie.getTotalTicketsBought())
+  });
+  for (let movie of movies)
+  if (movie.getTotalTicketsBought() > 0){
+    document.getElementById("stats").addEventListener("click", function(ev) {
+      getStats(movie)
+
+    })
+    getStats(movie)
+  }
+}
+function makeButton(movie) {
+  //console.log("Made button" + title + ":" + id);
+  const homePage = document.getElementById("homePage");
+  homePage.innerHTML += `
+      <button class="btn" id="${movie.id}">${movie.movieTitle}</button>
+  `;
+}
+
 function getMovie(id) {
   for (let movie of movies) {
     if (movie.id == id) return movie
@@ -78,30 +142,21 @@ function hide(elementId) {
 function unHide(elementId) {
   document.getElementById(elementId).classList.remove("hide")
 }
-
-homePage.innerHTML +=`
-<button id = "stats" class = "btn">Stats</button>`
+let i =0;
+  
 function getStats(movie) {
   
   let stats = document.createElement("div")
-  
-   
-  
+  if(completeTicketsBought > 0){
+    homePage.innerHTML +=`
+      <button id = "stats" class = "btn">Stats</button>`
+  }
   
   stats.innerHTML+=`
-  <ul>${movie.movieTitle} Adult tickets bought: ${movie.adultTicketsBought} Child tickets bought: ${movie.childTicketsBought} total tickets bought ${movie.getTotalTicketsBought()}</ul>`
+  <ul>${movie.movieTitle} Adult tickets bought: ${movie.adultTicketsBought} Child tickets bought: ${movie.childTicketsBought} total tickets bought ${movie.getTotalTicketsBought()}`
   
   
   hide("homePage")
-  
+  unHide("statsPage")
 
 }
-
-
-
-
-
-
-
-
-
